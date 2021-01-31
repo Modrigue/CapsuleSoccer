c675d2419d427ad90361af63cab1e1337b0dbda0
Modrigue <thealdobaldo@gmail.com>
2021-02-01 00:40:19 +0100

    1st version of offline mode refactoring in TS

--- CapsuleSoccerOffline/js/userInput.js
+++ CapsuleSoccerOffline/js/userInput.js
@@ -1,76 +1,75 @@
+"use strict";
 //Event listeners for the arrow keys
-function userInput1(obj){
-    canvas.addEventListener('keydown', function(e){
-        if(e.keyCode === 37){
+function userInput1(obj) {
+    canvas.addEventListener('keydown', function (e) {
+        if (e.keyCode === 37) {
             obj.left = true;
         }
-        if(e.keyCode === 38){
+        if (e.keyCode === 38) {
             obj.up = true;
         }
-        if(e.keyCode === 39){
+        if (e.keyCode === 39) {
             obj.right = true;
         }
-        if(e.keyCode === 40){
+        if (e.keyCode === 40) {
             obj.down = true;
         }
-        if(e.keyCode === 32){
+        if (e.keyCode === 32) {
             obj.action = true;
         }
     });
-    
-    canvas.addEventListener('keyup', function(e){
-        if(e.keyCode === 37){
+    canvas.addEventListener('keyup', function (e) {
+        if (e.keyCode === 37) {
             obj.left = false;
         }
-        if(e.keyCode === 38){
+        if (e.keyCode === 38) {
             obj.up = false;
         }
-        if(e.keyCode === 39){
+        if (e.keyCode === 39) {
             obj.right = false;
         }
-        if(e.keyCode === 40){
+        if (e.keyCode === 40) {
             obj.down = false;
         }
-        if(e.keyCode === 32){
+        if (e.keyCode === 32) {
             obj.action = false;
         }
-    });    
+    });
 }
-
-function userInput2(obj){
-    canvas.addEventListener('keydown', function(e){
-        if(e.keyCode === 65){
+function userInput2(obj) {
+    canvas.addEventListener('keydown', function (e) {
+        if (e.keyCode === 65) {
             obj.left = true;
         }
-        if(e.keyCode === 87){
+        if (e.keyCode === 87) {
             obj.up = true;
         }
-        if(e.keyCode === 68){
+        if (e.keyCode === 68) {
             obj.right = true;
         }
-        if(e.keyCode === 83){
+        if (e.keyCode === 83) {
             obj.down = true;
         }
-        if(e.keyCode === 9){
+        if (e.keyCode === 9) {
             obj.action = true;
         }
     });
-    
-    canvas.addEventListener('keyup', function(e){
-        if(e.keyCode === 65){
+    canvas.addEventListener('keyup', function (e) {
+        if (e.keyCode === 65) {
             obj.left = false;
         }
-        if(e.keyCode === 87){
+        if (e.keyCode === 87) {
             obj.up = false;
         }
-        if(e.keyCode === 68){
+        if (e.keyCode === 68) {
             obj.right = false;
         }
-        if(e.keyCode === 83){
+        if (e.keyCode === 83) {
             obj.down = false;
         }
-        if(e.keyCode === 9){
+        if (e.keyCode === 9) {
             obj.action = false;
         }
-    });    
+    });
 }
+//# sourceMappingURL=userInput.js.map
--- CapsuleSoccerOffline/ts/capsuleSoccer.ts
+++ CapsuleSoccerOffline/ts/capsuleSoccer.ts
@@ -1,55 +1,66 @@
-"use strict";
 //STEP 1: setting up the environment
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
+const ctx: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext('2d');
 //
 // creating the starting objects and variables here before starting the main loop
 // for example:
-class Player extends Capsule {
-    constructor() {
-        super(...arguments);
-        this.score = 0;
-    }
+
+class Player extends Capsule
+{
+    score: number = 0;
 }
-let player1 = new Player(80, 270, 150, 270, 25, 10);
+
+let player1: Player = new Player(80, 270, 150, 270, 25, 10);
 player1.maxSpeed = 4;
-let player2 = new Player(560, 270, 490, 270, 25, 10);
+let player2: Player = new Player(560, 270, 490, 270, 25, 10);
 player2.maxSpeed = 4;
 player1.score = 0;
 player2.score = 0;
-let football = new Ball(320, 270, 15, 6);
+
+let football: Ball = new Ball(320, 270, 15, 6);
+
 buildStadium();
+
 ctx.font = "30px Arial";
+
 //STEP 2: defining the game logic
-function gameLogic() {
-    if (football.pos.x < 45) {
-        scoring(player2);
+function gameLogic()
+{
+    if(football.pos.x < 45){
+        scoring(player2)
     }
-    if (football.pos.x > 595) {
-        scoring(player1);
+    if(football.pos.x > 595){
+        scoring(player1)
     }
-    if (player1.score === 3 || player2.score === 3) {
+    if(player1.score === 3 || player2.score === 3){
         gameOver();
     }
     ctx.fillText(player1.score.toString(), 20, 30);
     ctx.fillText(player2.score.toString(), 600, 30);
 }
+
 //STEP 3: handling the user input and the game loop
 userInput1(player1);
 userInput2(player2);
 requestAnimationFrame(mainLoop);
-function scoring(player) {
+
+function scoring(player: Player): void
+{
     player.score++;
+
     player1.vel.set(0, 0);
     player1.angVel = 0;
     player1.setPosition(100, 270, 0);
     player2.vel.set(0, 0);
     player2.angVel = 0;
     player2.setPosition(540, 270, 0);
+    
     football.vel.set(0, 0);
     football.setPosition(320, 270);
 }
-function gameOver() {
+
+function gameOver(): void
+{
     ctx.fillText("GAME OVER", 250, 30);
     player1.vel.set(0, 0);
     player1.angVel = 0;
@@ -62,15 +73,19 @@ function gameOver() {
     setTimeout(() => {
         player1.score = 0;
         player2.score = 0;
-    }, 2000);
+    }, 2000)
 }
-function buildStadium() {
+
+function buildStadium(): void
+{
     new Wall(60, 80, 580, 80);
     new Wall(60, 460, 580, 460);
+
     new Wall(60, 80, 60, 180);
     new Wall(60, 460, 60, 360);
     new Wall(580, 80, 580, 180);
     new Wall(580, 460, 580, 360);
+
     new Wall(50, 360, 10, 360);
     new Wall(0, 360, 0, 180);
     new Wall(10, 180, 50, 180);
@@ -78,5 +93,5 @@ function buildStadium() {
     new Wall(640, 360, 640, 180);
     new Wall(630, 180, 590, 180);
 }
+
 canvas.focus();
-//# sourceMappingURL=capsuleSoccer.js.map
